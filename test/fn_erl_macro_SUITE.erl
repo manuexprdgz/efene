2dea2798201798cd3744b317e0393825b1e5c2ef
David Martos <davidmartos96@gmail.com>
2018-02-22 17:06:52 +0100

    Clean macro SUITE

--- test/fn_erl_macro_SUITE.erl
+++ test/fn_erl_macro_SUITE.erl
@@ -11,6 +11,11 @@ init_per_suite(Config) ->
 end_per_suite(Config) ->
     Config.
 
+macros(Config) ->
+    MacroPath = macro_path(Config),
+    {ok, Macros} = fn_erl_macro:macro_defs(MacroPath),
+    Macros.
+
 macro_path(Config) ->
     DataDir = proplists:get_value(data_dir, Config),
     filename:join(DataDir, "ms.hrl").
@@ -21,36 +26,56 @@ print(Thing) ->
 exp(Macros, Key, Args) ->
     fn_erl_macro:expand_macro(Macros, Key, Args).
 
+
+%% Tests
+
 eval_macro(Config) ->
-    MacroPath = macro_path(Config),
-    {ok, Macros} = fn_erl_macro:macro_defs(MacroPath),
+    Macros = macros(Config),
+
     {ok, [{string, _, "bob"}]} = exp(Macros, 'AUTHOR', #{}),
+
     {ok, [{op, _, '*', {integer, _, 2}, {integer, _, 3}}]} = exp(Macros, 'Const', #{}),
 
     {ok, [{op, _, '+', {integer, _, 2}, {integer, _, 1}}]} = exp(Macros, {'Inc', 1}, #{'A' => {integer, 1, 2}}),
 
-    {ok, [{tuple, _, [{string, _, "bob"}, {integer, _, 42}]}]} = exp(Macros, {'AUTHOR', 1}, #{'A' => {integer, 1, 42}}),
+    { ok, [ {tuple, _, [{string, _, "bob"}, {integer, _, 42}]} ]
+    } = exp(Macros, {'AUTHOR', 1}, #{'A' => {integer, 1, 42}}),
 
-    {ok, [{tuple, _, [{atom, _, val}, {integer, 1, 1}, {integer, 2, 2}, {integer, 3, 3}]}]} = exp(Macros, {'V', 3}, #{'Line' => {integer, 1, 1}, 'Type' => {integer, 2, 2}, 'Val' => {integer, 3, 3}}),
 
-    R1 = exp(Macros, {'IncConst', 1}, #{'A' => {integer, 1, 42}}),
-    {ok, [{op, _, '+',
-           {integer, _, 42},
-           {op, _, '*',
-            {integer, _, 2},
-            {integer, _, 3}}}]} = R1.
+    R1 = exp(Macros, {'V', 3}, 
+                #{ 'Line' => {integer, 1, 1}, 
+                   'Type' => {integer, 2, 2}, 
+                   'Val' => {integer, 3, 3} 
+                }),
+    { ok, 
+      [{ tuple, _, 
+         [ {atom, _, val}, {integer, 1, 1}, {integer, 2, 2}, {integer, 3, 3} ]
+      }]
+    } = R1,
+
+
+    R2 = exp(Macros, {'IncConst', 1}, #{ 'A' => {integer, 1, 42} }),
+    { ok, 
+      [{ op, _, '+', {integer, _, 42}, 
+         { op, _, '*', {integer, _, 2}, {integer, _, 3} }
+      }]
+    } = R2.
 
 can_expand_var_with_ref(Config) ->
-    MacroPath = macro_path(Config),
-    {ok, Macros} = fn_erl_macro:macro_defs(MacroPath),
+    Macros = macros(Config),
     Ref = make_ref(),
-    {ok, [{op, _, '+', {var, 2, Ref}, {integer, _, 1}}]} = exp(Macros, {'Inc', 1}, #{'A' => {var, 2, Ref}}),
+    
+    { ok, 
+      [ {op, _, '+', {var, 2, Ref}, {integer, _, 1}} ]
+    } = exp(Macros, {'Inc', 1}, #{ 'A' => {var, 2, Ref} }),
+
     AstNode = {op, 1, '-', {integer, 1, 42}, {integer, 1, 43}},
-    {ok, [{op, _, '+', AstNode, {integer, _, 1}}]} = exp(Macros, {'Inc', 1}, #{'A' => AstNode}).
+    { ok, 
+      [ {op, _, '+', AstNode, {integer, _, 1}} ]
+    } = exp(Macros, {'Inc', 1}, #{'A' => AstNode}).
 
 can_expand_macro_str(Config) ->
-    MacroPath = macro_path(Config),
-    {ok, Macros} = fn_erl_macro:macro_defs(MacroPath),
+    Macros = macros(Config),
     AstNode = {op, 1, '-', {integer, 1, 42}, {integer, 1, 43}},
     {ok, [{string, _, "42 - 43"}]} = exp(Macros, {'Text', 1}, #{'Val' => AstNode}),
 
@@ -58,15 +83,13 @@ can_expand_macro_str(Config) ->
     "io:format(\"Call ~s: ~w~n\", [\"42 - 43\",42 - 43])" = lists:flatten(erl_pp:expr(Ast)).
 
 can_expand_macro_lex_str(Config) ->
-    MacroPath = macro_path(Config),
-    {ok, Macros} = fn_erl_macro:macro_defs(MacroPath),
+    Macros = macros(Config),
     AstNode = {string, 1, "foo"},
     {ok, [Ast]} = exp(Macros, {'NestedText', 1}, #{'A' => AstNode}),
     "\"foo\" ++ \"1 - 2\"" = lists:flatten(erl_pp:expr(Ast)).
 
 can_expand_simple_inner_macro_call(Config) ->
-    MacroPath = macro_path(Config),
-    {ok, Macros} = fn_erl_macro:macro_defs(MacroPath),
+    Macros = macros(Config),
     AstNode = {op, 1, '-', {integer, 1, 42}, {integer, 1, 43}},
     {ok, [Ast]} = exp(Macros, {'Int', 1}, #{'Val' => AstNode}),
     "{val,_,integer,42 - 43}" = lists:flatten(erl_pp:expr(Ast)),
@@ -75,8 +98,7 @@ can_expand_simple_inner_macro_call(Config) ->
     "{val,42 - 43,integer,12}" = lists:flatten(erl_pp:expr(Ast1)).
 
 can_expand_inner_macro_call_in_expr(Config) ->
-    MacroPath = macro_path(Config),
-    {ok, Macros} = fn_erl_macro:macro_defs(MacroPath),
+    Macros = macros(Config),
     AstNode = {op, 1, '-', {integer, 1, 42}, {integer, 1, 43}},
     {ok, [Ast]} = exp(Macros, {'Int1', 2}, #{'Line' => AstNode, 'Val' => {integer, 1, 12}}),
     "1 + {val,42 - 43,integer,12}" = lists:flatten(erl_pp:expr(Ast)).
