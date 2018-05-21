723348121cd7e1da8d6287663d8c431a15c5ede3
Manuel Exposito <manuel.exposito@udc.es>
2018-05-21 16:38:09 +0200

    pruebas de las primeras funciones del modulo

---
+++ myfnapp/README.rst
@@ -0,0 +1,28 @@
+myfnapp
+=====
+
+An efene application
+
+Build
+-----
+
+::
+
+    rebar3 compile
+
+Test
+----
+
+::
+
+    rebar3 ct
+
+TODO
+----
+
+* define license and create LICENSE file
+
+License
+-------
+
+TODO
--- test/fn_pp_SUITE.erl
+++ test/fn_pp_SUITE.erl
@@ -2,3 +2,26 @@
 -compile(export_all).
 -include_lib("proper/include/proper.hrl").
 
+all() -> [ind].
+
+% init_per_suite(Config) ->
+%     Config.
+
+% end_per_suite(Config) ->
+%     Config.
+
+list_ind(_Config) -> 0.
+
+match_A(A) -> 
+	if		
+		A == 0 -> "";
+		A == 1 -> "  ";
+		A == 2 -> "    ";
+		A == 3 -> "      ";
+		A == 4 -> "        "
+	end.
+
+
+ind(_Config) -> proper:quickcheck(?FORALL(A,list_ind(), match_A(A)==fn_pp:ind(A))).
+
+fmt(_Config) -> ["a", "", [104, "b"], "\n"] == fn_pp:fmt("a", "h", true, 0, "b").
