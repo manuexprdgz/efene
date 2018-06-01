e32d0f803cd540ba8522dec779244a2884cee9dd
Manuel Exposito <manuel.exposito@udc.es>
2018-06-01 19:27:34 +0200

    prueba parcial de print_kvs

--- test/fn_pp_SUITE.erl
+++ test/fn_pp_SUITE.erl
@@ -8,7 +8,7 @@
 -compile(export_all).
 -include_lib("proper/include/proper.hrl").
 
-all() -> [ind, fmt].
+all() -> [ind, fmt, print_kvs].
 
 init_per_suite(Config) ->
      Config.
@@ -41,17 +41,27 @@ fmt(_Config) -> proper:quickcheck(fmt2()).
 
 type_gen() -> ?LET({T},{elements(['s'])},{T}).
 
-select_gen(Type) when Type == 's' -> "A".
+select_gen(Type) -> 
+	if Type =:= 's' -> "A";
+	   Type =:= 'c' -> $a
+	end.
 
  
 
-args_gen() -> Type = elements(['s']),
-		?LET({Str,Args, T}, {string(),select_gen('s'),Type},
-		{Str,Args, "~10.5"++T}).
+args_gen() -> 
+		?LET({Str, T}, {string(), elements(['s', 'c'])},
+		{Str,select_gen(T), "~10.5"++T}).
 
 fmt_gen() -> ?LET({Format},{type_gen()},{Format, io:format(Format, ["A"], "A")}).
 
 fmt2() ->
 	?FORALL({Str,Args,Fmt}, args_gen(), [Str, "",io:format(Fmt, [Args]), "\n"] == fn_pp:fmt(Str, Fmt, true, 0, Args)).
 
-% print_kvs(_Config) -> []
+
+print_kvs(_Config) -> proper:quickcheck(print_kvs_test()).
+
+print_kvs_test() -> ?FORALL({Items}, list(any()), print_kvs_result(Items) == fn_pp:print_kvs(Items)).
+
+print_kvs_result([])  -> "";
+print_kvs_result([H]) -> fn_pp:print(H, "", false, 0);
+print_kvs_result([H|T]) -> fn_pp:print([H|T], "", false, 0, [], "", "").
